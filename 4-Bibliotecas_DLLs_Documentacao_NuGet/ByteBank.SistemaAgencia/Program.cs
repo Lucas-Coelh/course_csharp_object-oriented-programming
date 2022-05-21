cd6b490632c99410c6c9da5fc2ae3eb84f94e04f
Lucas Coelho <lucasfcoelho@ciandt.com>
2022-05-21 01:53:42 -0300

    Fim Parte 5: Bibliotecas_DLLs_Documentacao_NuGet

---
+++ 4-Bibliotecas_DLLs_Documentacao_NuGet/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2022, 5, 22);
+            DateTime dataCorrente = DateTime.Now;
+
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            Console.WriteLine(dataFimPagamento);
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine($"Tempo até o vencimento é de {TimeSpanHumanizeExtensions.Humanize(diferenca)}");
+
+            Console.ReadLine();
+        }
+
+    }
+}
